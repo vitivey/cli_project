44ebea76b2d58e0ce6a5464b4cab622b063f7070
Zindzi <zindzi@neighbour-neighbour.com>
2018-09-06 16:24:53 -0400

    Completed GEM

--- ttevents/lib/cli.rb
+++ ttevents/lib/cli.rb
@@ -1,8 +1,43 @@
 class CLI
 
     def self.run
-        puts "What's happening around town? Enter 'list' to see a list of events"
+        puts "What's happening around town? Let's find out!"
+        CLI.menu
         Search.new.search_events
     end
+
     
+    def self.details(index) #how do I make this method private? doesn't seem to work
+        puts "Event: #{Events.all[index].name}"
+        puts "Date and Time: #{Events.all[index].date}"
+        puts "Location: #{Events.all[index].location}"
+        puts "Phone: #{Events.all[index].phone}" if !Events.all[index].phone.empty?
+        puts "Category: #{Events.all[index].category}" if !Events.all[index].category.empty?
+        puts "Details: #{Events.all[index].details}" if !Events.all[index].details.empty?
+        puts "" #is there an alternative to putting blank line for extra space?
+        puts "Enter the event number to see more details on another event"
+    end
+    
+    def self.exit
+        puts ""
+        puts "Thank you! Goodbye!"
+    end
+
+    def self.invalid
+        puts ""
+        puts "Invalid input!"
+        puts "Enter an event number between 1-#{Events.all.size} to see more details on a particular event"
+    end
+
+    def self.list_selection
+        puts ""
+        puts "What event interests you?"
+        puts "Enter the event number to see more details on a particular event"
+        puts "Enter 'exit' to end session"
+    end
+
+    def self.menu
+        puts "Enter 'list' to see list of events"
+        puts "Enter 'exit' to end session"
+    end
 end
--- ttevents/lib/search.rb
+++ ttevents/lib/search.rb
@@ -1,7 +1,7 @@
 class Search
 
     def load_events  #loads all event data when search
-        Events.create(Scraper.summary_results)
+        Events.create(Scraper.summary_results) if Events.all.empty?
     end
 
     def search_events
@@ -9,29 +9,29 @@ class Search
         if input.strip== "list"
             load_events
             list
+            CLI.list_selection
+            search_events
         elsif input.strip=="exit"
-            puts "Thank you! Goodbye!"
-        elsif input.to_i > 0 #i.e., it is an integer
+            CLI.exit
+        elsif input.to_i > 0 && input.to_i <= Events.all.size #i.e., it is an integer
             search_results(input)
+            CLI.menu
+            search_events
         else
-            puts "Invalid input"
-            CLI.new
+            CLI.invalid
+            CLI.menu
+            search_events
         end
     end
 
-    def list
-        #User types 'list' and sees list upcoming events
+    def list         #User types 'list' and sees list upcoming events
         Events.all.each_with_index do |event, index|
-            puts "#{index+1}. #{event.name} - #{event.date}"
+            puts "#{index+1}. #{event.name} - #{event.location}"
         end
-        puts "What event interests you?"
-        puts "Enter number to show details on a specific event, or enter 'exit' to end session."
-        search_events
     end
 
     def search_results(input) #run the search and loops
         event_details(convert_input(input))
-        search_events
     end
 
     def convert_input(input) #converts index to event number
@@ -39,17 +39,9 @@ class Search
     end
 
     def event_details(index) #returns the details of one event
-        # event_id=convert_input(input)
-        Events.add_event_details(Events.all[index])
-        puts "name: #{Events.all[index].name}"
-        puts "date: #{Events.all[index].date}"
-        puts "location: #{Events.all[index].location}"
-        puts "phone: #{Events.all[index].phone}" if !Events.all[index].phone.empty?
-        puts "category: #{Events.all[index].category}" if !Events.all[index].category.empty?
-        puts "details: #{Events.all[index].details}" if !Events.all[index].details.empty?
+        Events.add_event_details(Events.all[index]) #updates event with additional details
+        CLI.details(index)
     end
-
-    
 
 
 
