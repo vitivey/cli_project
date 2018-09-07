67aafd7f75405c2ba28b7802952cda3c0e168221
Zindzi <zindzi@neighbour-neighbour.com>
2018-09-07 16:35:17 -0400

    Quick refactor

--- ttevents/lib/event.rb
+++ ttevents/lib/event.rb
@@ -3,17 +3,17 @@ class Events
     @@all=[]
 
     def initialize(event_hash)
-        event_hash.each {|key, value| self.send("#{key}=", value)} #adds name, dat, location and website to event
+        event_hash.each {|variable_name, variable_value| self.send("#{variable_name}=", variable_value)} #adds name, date, location and website to event
         save
     end
 
-    def self.create(search_results)  
+    def self.create(search_results)
         search_results.each {|event| Events.new(event)} #event is a hash of event details
     end
 
     def self.add_event_details(event_object)  #selected event is an event object
-        Scraper.event_details(event_object.website).each do |key, value|
-            event_object.send("#{key}=", value)
+        Scraper.event_details(event_object.website).each do |variable_name, variable_value|
+            event_object.send("#{variable_name}=", variable_value)
         end
     end
 
--- ttevents/lib/scraper.rb
+++ ttevents/lib/scraper.rb
@@ -3,31 +3,29 @@ class Scraper
     def self.summary_results #stores each event in a list (array)
         html=open("https://buzz.tt/events")
         html_nodes=Nokogiri::HTML(html)
-        results=[]
+        
         # html_nodes.search(".ev-card .card-det h4")[0].text
-        html_nodes.search(".ev-card").each do |event|
+        html_nodes.search(".ev-card").collect do |event|
             name = event.search(".card-det h4").text.strip
             date =  event.search(".card-det p").first.text.strip
             location = event.search(".card-det .ev-venue").first.text.strip
             website= "https://buzz.tt#{event.search(".card-det a").first.attr("href")}"
-            event_summary= {
+            {
                 :name => name,
                 :date => date,
                 :location => location,
                 :website => website
             }
-            results << event_summary
         end
-        results
     end
+
 
     def self.event_details(website) #stores event details in a hash
         html=open(website)
         html_nodes=Nokogiri::HTML(html)
 
-        event={}
-            phone=String.new
-            category=String.new
+        phone=String.new
+        category=String.new
             count=0
             while count < html_nodes.search(".ev-det-pg li").count
                 value = html_nodes.search(".ev-det-pg li")[count]
