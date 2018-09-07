f8c89e07bdf01adf859091edf4488a848cb2bc80
Zindzi <zindzi@neighbour-neighbour.com>
2018-09-07 18:16:56 -0400

    Refactored search and cli

--- ttevents/lib/search.rb
+++ ttevents/lib/search.rb
@@ -5,26 +5,25 @@ class Search
     end
 
     def search_events
+        menu
         input=gets
         if input.strip== "list"
             load_events
-            list
-            CLI.list_selection
+            list_of_events
+            list_event_selection
             search_events
         elsif input.strip=="exit"
-            CLI.exit
+            end_search
         elsif input.to_i > 0 && input.to_i <= Events.all.size #i.e., it is an integer
             search_results(input)
-            CLI.menu
             search_events
         else
-            CLI.invalid
-            CLI.menu
+            invalid
             search_events
         end
     end
 
-    def list         #User types 'list' and sees list upcoming events
+    def list_of_events         #User types 'list' and sees list upcoming events
         Events.all.each_with_index do |event, index|
             puts "#{index+1}. #{event.name} - #{event.location}"
         end
@@ -40,9 +39,39 @@ class Search
 
     def event_details(index) #returns the details of one event
         Events.add_event_details(Events.all[index]) #updates event with additional details
-        CLI.details(index)
+        search_details(index)
     end
 
+    def search_details(index) #how do I make this method private? doesn't seem to work
+        puts "Event: #{Events.all[index].name}"
+        puts "Date and Time: #{Events.all[index].date}"
+        puts "Location: #{Events.all[index].location}"
+        puts "Phone: #{Events.all[index].phone}" if !Events.all[index].phone.empty?
+        puts "Category: #{Events.all[index].category}" if !Events.all[index].category.empty?
+        puts "Details: #{Events.all[index].details}" if !Events.all[index].details.empty?
+        puts "" #is there an alternative to putting blank line for extra space?
+        puts "Enter the event number to see more details on another event"
+    end
+    
+    def end_search
+        puts ""
+        puts "Thank you! Goodbye!"
+    end
 
+    def invalid
+        puts ""
+        puts "Invalid input!"
+        puts "Enter an event number between 1-#{Events.all.size} to see more details on a particular event"
+    end
 
+    def list_event_selection
+        puts ""
+        puts "What event interests you?"
+        puts "Enter the event number to see more details on a particular event"
+    end
+
+    def menu
+        puts "Enter 'list' to see list of events"
+        puts "Enter 'exit' to end session"
+    end
 end
